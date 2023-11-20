8fb1453d8bbb00b8ba32347e5c31b54ab23ee8ba
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-11-20 21:18:42 +0100

    added eventListners

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ let screenOffset;
 
 let platforms = [];
 let sticks = [];
+
+let score = 0;
+
+const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d")
+
+const scoreElement = document.getElementById("score")
+const restartButtonElement = document.getElementById("restart")
+
+function resetGame() {
+
+}
+
+window.addEventListener("mousedown", function (event) {
+
+})
+
+window.addEventListener("mouseup", function (event) {
+    
+})
+
+function animate(timeStamp) {
+
+}
