d145d0138d1600c4c438ccf81755d802d7218865
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-12-01 18:34:24 +0100

    set up resetGame function

--- script.js
+++ script.js
@@ -16,7 +16,37 @@ const ctx = canvas.getContext("2d")
 const scoreElement = document.getElementById("score")
 const restartButtonElement = document.getElementById("restart")
 
+resetGame()
+
 function resetGame() {
+    gamePhase = "wating";
+    lastTimeStamp = undefined
+
+    platforms = [{ x: 50, w: 50}];
+
+    generatePlatform();
+    generatePlatform();
+    generatePlatform();
+    generatePlatform();
+   
+    //initializing hero position
+    heroX = platforms[0].x + platforms[0].w - 30;
+    heroY = 0;
+
+    //screen will stay centered with the hero
+    screenOffset = 0;
+
+    sticks = [{x: platforms[0].x + platforms[0].w, length: 0, rotation: 0}];
+
+    score = 0;
+
+    restartButtonElement.style.display = "none";
+    scoreElement.innerText = score;
+
+    draw()
+}
+
+function draw() {
 
 }
 
